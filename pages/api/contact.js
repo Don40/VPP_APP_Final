978d63db74e73f29da672993b826854258b7de77
dawn <87707102+Don40@users.noreply.github.com>
2024-12-17 17:56:58 +0530

    17th Dec2024 Where dbabse is using Prisma

---
+++ pages/api/contact.js
@@ -0,0 +1,29 @@
+// /pages/api/contact.js
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { firstName, lastName, constituency, village, contact } = req.body;
+
+      const newContact = await prisma.contact.create({
+        data: {
+          firstName,
+          lastName,
+          constituency,
+          village,
+          contact,
+        },
+      });
+
+      res.status(200).json({ message: "Contact saved successfully!", contact: newContact });
+    } catch (error) {
+      console.error("Error saving contact:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
